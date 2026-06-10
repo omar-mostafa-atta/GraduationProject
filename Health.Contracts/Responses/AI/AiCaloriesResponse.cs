b7c77d59955c1cfac6733a463d806c37b70fb776
omar mostafa <omarmostafaatta@gmail.com>
2026-06-10 12:31:04 +0300

    fixing ai response

--- Health.Contracts/Responses/AI/AiCaloriesResponse.cs
+++ Health.Contracts/Responses/AI/AiCaloriesResponse.cs
@@ -7,38 +7,42 @@ using System.Threading.Tasks;
 
 namespace Health.Contracts.Responses.AI
 {
-
-    //public class AiCaloriesResponse
-    //{
-    //    public int Calories { get; set; }
-    //    public string Food_Ar { get; set; }
-    //    public string Food_En { get; set; }
-    //    public string Source { get; set; }
-    //}
     public class AiCaloriesResponse
     {
+        [JsonPropertyName("data")]
         public AiCaloriesData Data { get; set; }
 
+        [JsonPropertyName("reply")]
         public string Reply { get; set; }
     }
 
     public class AiCaloriesData
     {
+        [JsonPropertyName("calculation_method")]
         public string CalculationMethod { get; set; }
 
+        [JsonPropertyName("calories_per_100g")]
         public double CaloriesPer100g { get; set; }
 
+        [JsonPropertyName("confidence")]
         public double Confidence { get; set; }
 
+        [JsonPropertyName("estimated_weight_g")]
         public double EstimatedWeightG { get; set; }
 
+        [JsonPropertyName("food_ar")]
         public string FoodAr { get; set; }
 
+        [JsonPropertyName("food_en")]
         public string FoodEn { get; set; }
 
+        [JsonPropertyName("food_id")]
         public int FoodId { get; set; }
 
+        [JsonPropertyName("requested_quantity")]
         public string RequestedQuantity { get; set; }
+
+        [JsonPropertyName("total_calories")]
         public double TotalCalories { get; set; }
     }
 }
